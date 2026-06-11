dc625cb6959f7c592ba3f4863b5be83090c9cbe8
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-06-11 11:17:53 +0530

    file cleaned up and basic file & folder structure done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the Car AI Assistant</h1>
+      <p>Your one-stop solution for all car-related queries.</p>
+    </div>
+  );
+};
+
+export default App;
